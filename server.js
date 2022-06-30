89398e601b37515a9eb28b87b0407b90b408ce27
Mihir Rathod <mihirrathod1612@gmail.com>
2022-06-30 16:09:27 +0530

    fixed errors

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const File = require("./models/File");
-const req = require("express/lib/request");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
@@ -12,10 +11,7 @@ const uploadFile = multer({dest : 'sharedFile'});
 
 const connectDB = async() => {
   try{
-      const conn = await mongoose.connect(process.env.DATABASE_URL , {
-          useNewUrlParser : true,
-          useUnifiedTopology : true
-      });
+      const conn = await mongoose.connect(process.env.DATABASE_URL);
       console.log(`MongoDB connected on ${conn.connection.host}`);
   }
   catch(error)
@@ -27,50 +23,49 @@ const connectDB = async() => {
 }
 connectDB();
 
-app.set('view engine' , 'ejs')
+app.set("view engine", "ejs")
 
-app.get('/' , (req,res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index")
 })
 
-app.post('/upload' , uploadFile.single('file') , async (req,res) => {
-    const fileData = {
-        path: req.file.path,
-        originalName: req.file.originalname,
-      }
-      if (req.body.pswd != null && req.body.pswd !== "") {
-        fileData.password = await bcrypt.hash(req.body.pswd, 10)
-      }
-    
-      const file = await File.create(fileData)
-    
-      res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
-    })
+app.post("/upload", uploadFile.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  }
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10)
+  }
 
+  const file = await File.create(fileData)
+
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
+})
 
 app.route("/file/:id").get(handleDownload).post(handleDownload)
 
-  async function handleDownload(req, res) {
-      const file = await File.findById(req.params.id)
-      
-      if (file.password != null) {
-        if (req.body.password == null) {
-          res.render("password")
-          return
-        }
-    
-        if (!(await bcrypt.compare(req.body.password, file.password))) {
-          res.render("password", { error: true })
-          return
-        }
-      }
-    
-      file.downloadCount++
-      await file.save()
-    
-      res.download(file.path, file.originalName)
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id)
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password")
+      return
+    }
+
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("password", { error: true })
+      return
+    }
   }
+
+  file.downloadCount++
+  await file.save()
+
+  res.download(file.path, file.originalName)
+}
 
 app.listen(process.env.PORT , ()=>{
   console.log(`Server running on port no ${process.env.PORT}`);
-})
+});
